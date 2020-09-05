0fb9f8b0e22148c14675e820cf9eb21673d44b7d
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-09-04 19:40:40 -0500

    auth

---
+++ ldaw-auth-base-master/ldaw-auth-base-master/server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let app = express();
+let webRoutes = require('./routes/web');
+
+/**
+ * Configurations
+ */
+
+let appConfig = require('./configs/app');
+
+// Views engine
+let exphbs = require('express-handlebars');
+// Imports a set of helpers for handlebars
+// https://github.com/helpers/handlebars-helpers
+let hbshelpers = require("handlebars-helpers");
+let multihelpers = hbshelpers();
+const extNameHbs = 'hbs';
+let hbs = exphbs.create({
+  extname: extNameHbs,
+  helpers: multihelpers
+});
+app.engine(extNameHbs, hbs.engine);
+app.set('view engine', extNameHbs);
+
+/**
+ * Routes
+ */
+app.use('/', webRoutes);
+
+/**
+ * App Init
+ */
+app.listen(appConfig.expressPort, () => {
+  console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+});
