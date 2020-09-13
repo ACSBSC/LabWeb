bf20d99875fabcfc5664c6878d6c883917d9b431
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-09-13 18:41:02 -0500

    face+ajax

---
+++ facebook-login-master/configs/passport.js
@@ -0,0 +1,71 @@
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+
+let FacebookStrategy = require('passport-facebook').Strategy;
+
+let UserModel = require('../models/User');
+let bcrypt = require('bcryptjs');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const userTableFields = {
+  usernameField: 'email',
+  passwordField: 'password'
+};
+
+const verifyCallback = (email, password, done) => {
+  UserModel.findByEmail(email)
+    .then((user) => {
+      // Si no encuentra un usuario entonces regresa falso
+      if (!user) {
+        return done(null, false);
+      }
+      // Si encuentra un usuario y coincide con la contraseña entonces
+      // inicia la sesión
+      let isValid = bcrypt.compareSync(password, user.password);
+      if (isValid) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => {
+      done(err);
+    });
+}
+
+const strategy  = new LocalStrategy(userTableFields, verifyCallback);
+passport.use(strategy);
+
+const fbConfigs = {
+  clientID: process.env.PASSPORT_FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.PASSPORT_FACEBOOK_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/fb/callback',
+  profileFields: ['email', 'name']
+};
+const fbStrategy = new FacebookStrategy(fbConfigs, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  UserModel.create({ name: profile.name.givenName, email: profile.name.givenName, password: profile.name.givenName })
+    .then((id) => {
+      return UserModel.find(id)
+        .then(user => done(null, user) )
+    });
+});
+passport.use(fbStrategy);
+
+
+
+// Guarda en las variables de sesión el id del usuario loggeado
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Obtiene el usuario
+passport.deserializeUser((id, done) => {
+  UserModel.find(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch(err => done(err))
+});
---
+++ facebook-login-master/routes/web.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const authController = require('../controllers/AuthController');
+const authMiddleware = require('../middlewares/AuthMiddleware');
+const authValidator = require('../validators/AuthValidators');
+const homepageController = require('../controllers/HomepageController');
+const passport = require('passport');
+
+router.get('/', homepageController.index);
+
+router.get('/auth/login', authController.login);
+router.get('/auth/register', authController.register);
+router.post('/auth/register', authValidator.store, authController.store);
+router.post('/auth/login', passport.authenticate('local', { failureRedirect: '/auth/login?authError=1', successRedirect: '/' }));
+router.get('/auth/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/protected', authMiddleware.isAuth, (req, res) => {
+  res.send('Protected route, user correctly authenticated');
+})
+
+
+
+router.get('/auth/fb/login', passport.authenticate('facebook'));
+router.get('/auth/fb/callback', passport.authenticate('facebook', {
+  successRedirect: '/auth/fb/success',
+  failureRedirect: '/auth/fb/fail'
+}));
+router.get('/auth/fb/success', (req, res) => {
+  res.send('Facebook success');
+});
+router.get('/auth/fb/fail', (req, res) => {
+  res.send('Facebook failed');
+});
+
+
+module.exports = router;
